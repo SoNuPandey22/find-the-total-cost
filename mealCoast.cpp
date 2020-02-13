ef11ffd22b6fbeb458b924f4229266aec796b911
sonu pandey <pandey.sonu1739@gmail.com>
2020-02-13 23:44:46 +0530

    Create mealCoast.cpp

---
+++ mealCoast.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the solve function below.
+void solve(double meal_cost, int tip_percent, int tax_percent) {
+    float tip = (meal_cost / 100) * tip_percent;
+    float tax = (meal_cost /100) *tax_percent;
+    float total_cost = (meal_cost + tip + tax);
+    cout<<fixed<<setprecision(0)<<total_cost<<endl;
+
+
+}
+
+int main()
+{
+    double meal_cost;
+    cin >> meal_cost;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int tip_percent;
+    cin >> tip_percent;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int tax_percent;
+    cin >> tax_percent;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    solve(meal_cost, tip_percent, tax_percent);
+
+    return 0;
+}
